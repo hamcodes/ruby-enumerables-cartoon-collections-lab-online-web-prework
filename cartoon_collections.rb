ecdc8b3874707331a44da412e2770c772587fdfa
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-02 22:52:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,4 +17,8 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |x|
+    if cheese_types.include?(x)
+      return x
+    end
 end
